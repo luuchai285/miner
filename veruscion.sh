21f81e7647143570640abc228c9ed19e9aad9ca6
luuchai285 <83363836+luuchai285@users.noreply.github.com>
2021-07-12 21:41:35 +0700

    Create veruscion.sh

---
+++ veruscion.sh
@@ -0,0 +1,5 @@
+# verucoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RYA7KnefsWqTVLtwBk1cFEeDD53Z1YEjav.linux1 -p x --cpu 4
